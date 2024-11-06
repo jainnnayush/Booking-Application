43444c681f873c9434ef726b9996170daf3a7444
Ayush Jain <ayush.cl21@iitg.ac.in>
2024-11-06 18:25:13 +0530

    Search Bar+MailList

--- Client/src/pages/home/Home.jsx
+++ Client/src/pages/home/Home.jsx
@@ -1,14 +1,26 @@
-import React from 'react'
-import Navbar from '../../components/navbar/Navbar'
-import Header from '../../components/header/Header'
+import Featured from "../../components/featured/Featured";
+import FeaturedProperties from "../../components/featuredProperties/FeaturedProperties";
+import Header from "../../components/header/Header";
+import MailList from "../../components/mailList/mailList";
+import Navbar from "../../components/navbar/Navbar";
+import PropertyList from "../../components/propertyList/PropertyList";
+import "./home.css";
 
 const Home = () => {
   return (
     <div>
-      <Navbar/>
+      <Navbar />
       <Header/>
+      <div className="homeContainer">
+        <Featured/>
+        <h1 className="homeTitle">Browse by property type</h1>
+        <PropertyList/>
+        <h1 className="homeTitle">Homes guests love</h1>
+        <FeaturedProperties/>
+        <MailList/>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
