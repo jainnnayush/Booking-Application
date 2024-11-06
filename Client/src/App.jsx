fe67be1b47fd0c5e764062fe90e42c0bfc75c9bd
Ayush Jain <ayush.cl21@iitg.ac.in>
2024-11-06 21:17:40 +0530

    hotel design

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -3,10 +3,9 @@ import {
   Routes,
   Route,
 } from "react-router-dom";
-import Home from "./pages/home/Home";
+import Home from "./pages/home/home";
 import Hotel from "./pages/hotel/Hotel";
 import List from "./pages/list/List";
-import Login from "./pages/login/Login";
 
 function App() {
   return (
@@ -15,7 +14,6 @@ function App() {
         <Route path="/" element={<Home/>}/>
         <Route path="/hotels" element={<List/>}/>
         <Route path="/hotels/:id" element={<Hotel/>}/>
-        <Route path="/login" element={<Login/>}/>
       </Routes>
     </BrowserRouter>
   );
